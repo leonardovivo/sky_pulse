c1675221a2ba3b04b40929a26c87c50b75bb9cd0
leonardovivo <leonardovivo3@gmail.com>
2024-07-13 20:26:49 -0300

    Imagem de fundo colocada na tela, adição de novo Text e alteração dos textos para inglês

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -3,6 +3,7 @@ import '../controllers/text_field_controller.dart';
 
 class CityScreen extends StatefulWidget {
   const CityScreen({super.key});
+
   @override
   _CityScreenState createState() => _CityScreenState();
 }
@@ -19,62 +20,89 @@ class _CityScreenState extends State<CityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Padding(
-          padding: EdgeInsets.only(top: 30.0),
-          child: Text(
-            'Welcome',
-            style: TextStyle(
-              color: Color.fromARGB(255, 2, 179, 255),
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+            height: constraints.maxHeight,
+            width: constraints.maxWidth,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "assets/images/the_background.jpeg",
+                ),
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset('assets/images/logotipo.png'),
-            const SizedBox(height: 16.0),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(40.0),
-                child: TextField(
-                  controller: _textFieldController.controller,
-                  decoration: const InputDecoration(
-                    labelText: 'Digite a cidade...',
-                    fillColor: Colors.white,
-                    filled: true,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  AppBar(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    title: const Padding(
+                      padding: EdgeInsets.only(top: 30.0),
+                      child: Text(
+                        'Welcome to',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    centerTitle: true,
                   ),
-                ),
+                  Image.asset('assets/images/logotipo.png'),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 16.0),
+                    child: Text(
+                      'City weather check',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 10.0, left: 16.0, right: 16.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(40.0),
+                      child: TextField(
+                        controller: _textFieldController.controller,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter a city...',
+                          fillColor: Colors.white,
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: () {
+                      print(
+                          'Texto inserido: ${_textFieldController.controller.text}');
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all<Color>(
+                          const Color.fromARGB(255, 2, 179, 255)),
+                    ),
+                    child: const Text(
+                      'Check weather',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                print(
-                    'Texto inserido: ${_textFieldController.controller.text}');
-              },
-              style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all<Color>(
-                    Color.fromARGB(255, 2, 179, 255)),
-              ),
-              child: Text(
-                'Informar clima',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }

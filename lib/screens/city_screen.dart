be81dd905bd6f0d8f4dfb822a415954b22c0c84b
leonardovivo <leonardovivo3@gmail.com>
2024-07-12 23:43:30 -0300

    Logotipo, TextField e botão que trocará de tela adicionados

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import '../controllers/text_field_controller.dart';
 
-class CityScreen extends StatelessWidget {
+class CityScreen extends StatefulWidget {
   const CityScreen({super.key});
+  @override
+  _CityScreenState createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  final TextFieldController _textFieldController = TextFieldController();
+
+  @override
+  void dispose() {
+    _textFieldController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +27,7 @@ class CityScreen extends StatelessWidget {
           child: Text(
             'Welcome',
             style: TextStyle(
-              color: Color.fromARGB(255, 0, 255, 191),
+              color: Color.fromARGB(255, 2, 179, 255),
               fontSize: 30,
               fontWeight: FontWeight.bold,
             ),
@@ -22,10 +35,45 @@ class CityScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'Olá, Mundo!',
-          style: TextStyle(fontSize: 24, color: Colors.white),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/logotipo.png'),
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(40.0),
+                child: TextField(
+                  controller: _textFieldController.controller,
+                  decoration: const InputDecoration(
+                    labelText: 'Digite a cidade...',
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                print(
+                    'Texto inserido: ${_textFieldController.controller.text}');
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(
+                    Color.fromARGB(255, 2, 179, 255)),
+              ),
+              child: Text(
+                'Informar clima',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );

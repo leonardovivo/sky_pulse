208a62ead5740a1406b173cc39b0a3334a53733a
leonardovivo <leonardovivo3@gmail.com>
2024-07-21 21:21:33 -0300

    Define estados BLoC para gerenciamento de dados meteorológicos

---
+++ lib/bloc/weather_state.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:sky_pulse/models/weather_model.dart';
+
+abstract class WeatherState extends Equatable {
+  const WeatherState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class WeatherInitial extends WeatherState {}
+
+class WeatherLoading extends WeatherState {}
+
+class WeatherLoaded extends WeatherState {
+  final Weather weather;
+
+  const WeatherLoaded({required this.weather});
+
+  @override
+  List<Object> get props => [weather];
+}
+
+class WeatherError extends WeatherState {
+  final String message;
+
+  const WeatherError({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
